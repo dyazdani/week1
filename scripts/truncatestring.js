2a44a6747d403437bed1047900f9765ba1d81ecb
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-01-06 14:59:48 -0800

    adding solution for truncateStrin() fCC problem

---
+++ scripts/truncatestring.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//Big O = O(n)
+
+// Truncate a string (first argument) if it is longer than the given maximum string 
+// length (second argument). Return the truncated string with a ... ending.
+
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        return str.slice(0, num) + "...";
+    } else {
+        return str;
+    }
+  }
+
+module.exports = {truncateString};
