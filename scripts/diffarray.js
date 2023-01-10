0761421c8c44ec8aae123da0c8bdf5997a1f580c
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-01-10 07:29:27 -0800

    amending solution for diffArray() fCC problem

--- scripts/diffarray.js
+++ scripts/diffarray.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//TODO Big O = ?
+//TODO Big O = O(n)
 
 // Diff Two Arrays
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
